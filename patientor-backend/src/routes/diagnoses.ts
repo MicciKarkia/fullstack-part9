beca365b819aa8da3417ff66bd026b5bc8c4f08f
MicciKarkia <michaela.karkia@ksfmedia.fi>
2023-11-14 15:47:54 +0200

    Exercise 9.10

--- patientor-backend/src/routes/diagnoses.ts
+++ patientor-backend/src/routes/diagnoses.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import diagnosesService from '../services/diagnosesService';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all diagnoses');
+  res.send(diagnosesService.getDiagnoses());
 });
 
 export default router;
